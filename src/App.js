7807237b6693605b0d35f8affd5c1f0e73c157a3
karla meneses <parrakarla656@gmail.com>
2022-04-03 14:03:21 -0500

    otro intento 4

--- src/App.js
+++ src/App.js
@@ -24,24 +24,7 @@ const App = () => {
   }
 
  useEffect(() => {
-   const getAllPokemonsPivot = () => (
-  async () => {
-    const res = await fetch(loadMore)
-    const data = await res.json()
-
-    setLoadMore(data.next)
-
-    function createPokemonObject(results)  {
-      results.forEach( async pokemon => {
-        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
-        const data =  await res.json()
-        setAllPokemons( currentList => [...currentList, data])
-        await allPokemons.sort((a, b) => a.id - b.id)
-      })
-    }
-    createPokemonObject(data.results)
-  })
-   getAllPokemonsPivot()()
+   getAllPokemons()
 }, [getAllPokemons])
 
   return (
