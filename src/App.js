aba76968c8a619628a6653a5db3053e228b64e11
karla meneses <parrakarla656@gmail.com>
2022-04-03 14:28:39 -0500

    otro intento 12

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ const App = () => {
         const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
         const data =  await res.json()
         setAllPokemons( currentList => [...currentList, data])
-        await allPokemons.sort((a, b) => a.id - b.id)
+        /*await allPokemons.sort((a, b) => a.id - b.id)*/
       })
     }
     createPokemonObject(data.results)
