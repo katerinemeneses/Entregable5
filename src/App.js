a613724743dc86ca389e76dc136bb572676138b0
karla meneses <parrakarla656@gmail.com>
2022-04-03 14:21:50 -0500

    otro intento 9

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
    const[allPokemons, setAllPokemons] = useState([])
    const [loadMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon?limit=20')
 
-  const getAllPokemons = async () => {
+  /*const getAllPokemons = async () => {
     const res = await fetch(loadMore)
     const data = await res.json()
 
@@ -17,14 +17,29 @@ const App = () => {
         const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
         const data =  await res.json()
         setAllPokemons( currentList => [...currentList, data])
-        /*await allPokemons.sort((a, b) => a.id - b.id)*/
+        await allPokemons.sort((a, b) => a.id - b.id)
       })
     }
     createPokemonObject(data.results)
-  }
+  }*/
 
- useEffect((getAllPokemon = getAllPokemons) => {
-   getAllPokemon()
+ useEffect(() => {
+  (async () => {
+    const res = await fetch(loadMore)
+    const data = await res.json()
+
+    setLoadMore(data.next)
+
+    function createPokemonObject(results)  {
+      results.forEach( async pokemon => {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+        const data =  await res.json()
+        setAllPokemons( currentList => [...currentList, data])
+        await allPokemons.sort((a, b) => a.id - b.id)
+      })
+    }
+    createPokemonObject(data.results)
+  })()
 }, [])
 
   return (
