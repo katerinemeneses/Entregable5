29ae095adfcf41000b5d830a82ff311fe0eaa915
karla meneses <parrakarla656@gmail.com>
2022-04-03 13:15:12 -0500

    fin

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import PokemonThumb from './components/PokemonThumb'
-import PokemonDetails from './components/PokemonDetails'
-
+ 
 const App = () => {
 
    const[allPokemons, setAllPokemons] = useState([])
